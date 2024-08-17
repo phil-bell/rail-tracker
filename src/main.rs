da08ba73d6cf4512363327bbb09175c875a719d6
phil-bell <philhabell@gmail.com>
2024-08-17 13:32:08 +0000

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
+
+.io/api/v1/json/search/LMS")
+
+
+556070bcdea17cf74d0d8f15f"),
+
+
+
+alue>()
+
+
     let res = client
-        .get("https://api.rtt.io/api/v1/json/service/C79960/2024/08/17")
-        .basic_auth("", Some(""))
+        .get(
+            "https://api.rtt.io/api/v1/json/service/C79960/2024/08/17
+",
+        )
+        .basic_auth(
+            "",
+            Some(""),
+        )
         .send()
         .await?
         .json::<serde_json::Value>()
