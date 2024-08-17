5e5744e069a36e72456bbf86fd4886e7eadac952
Phil Bell <phil@legl.com>
2024-08-17 17:38:46 +0100

    getting creds from env

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
+use std::env;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let username = match env::var_os("RTT_USERNAME") {
+        Some(v) => v.into_string().unwrap(),
+        None => panic!("$RTT_USERNAME is not set")
+    };
+    let secret = match env::var_os("RTT_SECRET") {
+        Some(v) => v.into_string().unwrap(),
+        None => panic!("$RTT_SECRET is not set")
+    };
     let client = reqwest::Client::new();
-
-.io/api/v1/json/search/LMS")
-
-
-556070bcdea17cf74d0d8f15f"),
-
-
-
-alue>()
-
-
     let res = client
         .get(
             "https://api.rtt.io/api/v1/json/service/C79960/2024/08/17
 ",
         )
-        .basic_auth(
-            "",
-            Some(""),
-        )
+        .basic_auth(username, Some(secret))
         .send()
         .await?
         .json::<serde_json::Value>()
