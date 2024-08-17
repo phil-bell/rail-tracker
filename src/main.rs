00ac27fa2b217b83660438f908fac4be094c48f1
phil-bell <philhabell@gmail.com>
2024-08-17 13:21:23 +0000

    adding call to service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let res = client
+        .get("https://api.rtt.io/api/v1/json/service/C79960/2024/08/17")
+        .basic_auth("", Some(""))
+        .send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+    println!("{res:#?}");
+
+    Ok(())
 }
